5b8d6362218ba708150cb2601b63638cf8199950
Param Rathour <rathourparam3435@gmail.com>
2023-10-12 02:07:14 +0530

    Added Project
    Thanks @wermos for suggestions

---
+++ README.md
@@ -0,0 +1,74 @@
+A Python Script which generates a grid of images. It also accounts for the ordering between these images provided the user specifies one.
+
+The motivation behind this project is to combine all memorable images into one image while also exploiting their order of importance. For eg., this is useful when one wants to combine their favourite moment from each anime they have watched into a grid and they also want to arrange these pictures considering the amount of personal impact.
+
+![example anime grid](https://raw.githubusercontent.com/paramrathour/website-assets/main/anime-grid.jpg)
+
+## Working
+- It concatenates all the images vertically and horizontally to form a square grid of appropriate size. If a square grid is not possible then we fill empty places with blank images.
+
+- If an ordering is provided, these images can be put in a normal row-major or horizontal snake-like or a counterclockwise spiral pattern. By passing `invert-chirality` parameter, the analogous column-major or vertical snake-like or a clockwise spiral pattern.
+
+- Then it outputs the original and resized (compressed) version of this image grid. It also outputs a text file containing captions which specifies the image names from left to right, top to bottom in the grid. 
+
+- The idea here is to use two files, one containing short abbreviations (image names) that map to their complete name (used in caption names). This as opposed to using image file names themselves as captions allows us to have caption names that were otherwise not allowed by the filesystem.
+
+## Requirements
+- Python 3 with libraries `NumPy`, `OpenCV`.
+- A folder containing images in `png`/`jpg`/`jpeg` format.
+- A file `image-names.txt` file which contains the corresponding image names on each line (preferably ordered). Only the image names contained here will be used.
+- A file `image-captions.txt` which maps image names to caption names (can be in any order) in the format `<image-name> | <anime-name>`. 
+
+## Example
+Consider the pictures of Mandelbrot set with different zooom iterations. Then the iteration number is a suitable image name ([file](Example/image-names.txt)), and the names describing each iteration are suitable caption names ([file](Example/image-captions.txt)).
+
+An example command to generate grid for images in the example directory is as below
+```
+python grid-generator.py --image-source 'Example/Pics/' --image-names 'Example/image-names.txt'
+--image-captions 'Example/image-captions.txt' --image-destination 'Example/Outputs/'
+--image-dimension '2560 1920' --grid-type 'snake' --save-jpg
+```
+This generates `jpg` images of the resultant horizontal snake-like grid pattern, where each image in grid is of size $2560\times1920$.
+
+<img src="Example/Outputs/4by4.jpg" alt="Output Image" style="width=100%"/>
+
+## Options
+#### Required Parameters
+    --image-source               Folder path: source of images ('png'/'jpg'/'jpeg')
+    --image-destination          Folder path: destination of generated grid images
+    --image-names                File: name of images
+    --image-captions             File: map of image names to caption names
+    --image-dimension            Integer Pair: <image-width image-height>
+    --grid-type                  String: Either 'normal' or 'snake' or 'spiral'
+#### Optional Parameters
+    --grid-dimension             Integer Pair: <grid-width grid-height> for the compressed images
+    --grid-size                  Integer: size of image grid (n images by n images)
+    --force-odd-size             Pass if desired grid dimension is odd, will keep the first ordered
+    --grayscale-images           Pass if images are grayscale
+    --invert-chirality           Pass if a grid with inverse chirality is required
+    --save-png                   Pass if desired output image extension is 'png'
+    --save-jpg                   Pass if desired output image extension is 'jpg'
+    --black-blank-image          Pass if desired blank images are required to be all black
+                                 image exactly at center in case of a spiral grid
+
+For image cropping (0-based-indexing of pixel coordinates)
+
+    --top-left-pixel             Integer Pair: <x-coordinate y-coordinate> of top left pixel
+    --bottom-right-pixel         Integer Pair: <x-coordinate y-coordinate> of bottom right pixel
+
+### Behaviour without optional commands
+- No arguments need to be supplied when passing `grayscale-images`, `invert-chirality`, `save-png`, `save-jpg`, `force-odd-size`, `black-blank-image`.
+- If at least one of `grid-width` and `grid-height` are not provided then the compressed output grid image will be the largest image of size less than 10 megapixels with the same scale as input image dimensions unless the given image size itself is greater than 10 megapixels and has an irreducable aspect ratio.
+- If `grid-dimension` is not provided then it will automatically generate the smallest square grid that can fit all the given images and fill empty spots with blank images.
+- If `grayscale-images` is not provided then the default behaviour is to assume all images are colored.
+- If none of `save-jpg` and `save-png` is provided then the default behaviour is to save `png` images.
+- If `black-blank-image` is not provided then the default behaviour is to assume blank images are all white.
+- The image will be cropped from top left pixel to bottom right pixel if both x-coordinate and y-coordinate are specified for each case.
+  - If only the top left pixel parameters are supplied then bottom right pixel is assumed to be the bottom right corner pixel.
+  - If only the bottom right pixel parameters are supplied then top left pixel is assumed to be the top left corner pixel.
+- Not supplying other optional arguments has no unintended consequences.
+
+### Limitations
+- All images should have same color format (i.e. either all images are grayscale, or all are colored).
+- All images must have same dimension (`image-width` by `image-height`) for best results, in case a file is not of given dimension then I will rescaled to `image-width` by `image-height`. Then after scaling, the provided cropping parameters will be considered.
+- All images need not have same extension but unique image names must have unique extensions corresponding to them.
---
+++ grid-generator.py
@@ -0,0 +1,207 @@
+import os, os.path
+import numpy as np
+import argparse as ap
+import math
+import csv
+import cv2
+
+parser = ap.ArgumentParser()
+parser.add_argument("--image-source")
+parser.add_argument("--image-names") # A file which contains the corresponding image names of anime on each line (preferably sorted)
+parser.add_argument("--image-captions") # Mapping image names to anime names (can be in any order).
+# File Format "<image_name> | <anime_name>", where "|" character is used as seperator as image_name is a file name which can't contain "|"
+parser.add_argument("--image-destination")
+parser.add_argument("--image-dimension")
+parser.add_argument("--grid-dimension") 
+parser.add_argument("--grid-size") 
+parser.add_argument("--force-odd-size", action = 'store_true')
+parser.add_argument("--grid-type")
+parser.add_argument("--grayscale-images", action = 'store_true')
+parser.add_argument("--invert-chirality", action = 'store_true')
+parser.add_argument("--save-png", action = 'store_true')
+parser.add_argument("--save-jpg", action = 'store_true')
+parser.add_argument("--black-blank-image", action = 'store_true')
+parser.add_argument("--top-left-pixel")
+parser.add_argument("--bottom-right-pixel")
+arguments = parser.parse_args()
+
+def parse_arguments(arguments):
+    number_of_pixels = 10000000
+    grid_size = arguments.grid_size
+    image_source = arguments.image_source
+    image_names = arguments.image_names
+    image_captions = arguments.image_captions
+    image_destination = arguments.image_destination
+    image_dimension  = arguments.image_dimension
+    grid_dimension  = arguments.grid_dimension
+    grayscale_images = bool(arguments.grayscale_images)
+    save_png = bool(arguments.save_png)
+    save_jpg = bool(arguments.save_jpg)
+    force_odd_size = bool(arguments.force_odd_size)
+    black_blank_image = bool(arguments.black_blank_image)
+    grid_type = arguments.grid_type
+    invert_chirality = bool(arguments.invert_chirality) 
+    top_left_pixel = arguments.top_left_pixel
+    bottom_right_pixel = arguments.bottom_right_pixel
+
+    if grayscale_images:
+        image_depth = 1
+    else:
+        image_depth = 3
+    if save_jpg == False and save_png == False:
+        save_png = True
+
+    image_dimension = image_dimension.split()
+    image_dimension = (int(image_dimension[0]), int(image_dimension[1]))
+    if grid_dimension == None:
+        scale = int(math.floor(math.sqrt(number_of_pixels/(image_dimension[0]*image_dimension[1]))))
+        scale += (scale == 0)
+        grid_dimension = (scale * image_dimension[0], scale * image_dimension[1]);
+    else:
+        grid_dimension = grid_dimension.split()
+        grid_dimension = (int(grid_dimension[0]), int(grid_dimension[1]))
+        
+    if top_left_pixel !=  None:
+        top_left_pixel = top_left_pixel.split()
+        top_left_pixel = (int(top_left_pixel[0]), int(top_left_pixel[1]))
+    else:
+        top_left_pixel = (0, 0)
+    if bottom_right_pixel !=  None:
+        bottom_right_pixel = bottom_right_pixel.split()
+        bottom_right_pixel = (int(bottom_right_pixel[0])+1, int(bottom_right_pixel[1])+1)
+    else:
+        bottom_right_pixel = (image_dimension[1], image_dimension[0])
+
+    if not os.path.exists(image_destination):
+        os.mkdir(image_destination)
+
+    names_all = []
+
+    # Blank Image (to be put if number of images is not a squares)
+    blank = "_"
+    captions_all = {blank:blank}
+    blank_image = np.zeros([image_dimension[1],image_dimension[0],image_depth], dtype = np.uint8)
+    blank_image.fill(0 if black_blank_image else 255)
+
+    with open(image_names) as filenames:
+        for line in filenames:
+            names_all.append(line.strip())
+
+    with open(image_captions, 'r') as filenames:
+        lines = csv.reader(filenames, delimiter = '|')
+        for line in lines:
+            captions_all[line[0].strip()] = line[1].strip()
+
+    if grid_size == None:
+        n = int(np.ceil(math.sqrt(len(names_all))))
+    else:
+        n = int(grid_size)
+
+    image_input_extension = ['png', 'jpg', 'jpeg']
+    return n, grid_type, invert_chirality, captions_all, names_all, blank, blank_image, image_source, image_destination, image_dimension, grid_dimension, top_left_pixel, bottom_right_pixel, image_input_extension, grayscale_images, save_png, save_jpg, force_odd_size
+
+def get_names(i, names, blank):
+    if i < len(names):
+        return names[i]
+    else:
+        return blank
+
+def updateGrid(grid, x, y, counter,n):
+    if x >=  0 and x < n  and y >=  0 and y < n:
+        grid[x][y] = counter
+    return counter+1
+
+def generate_grid(n, grid_type, invert_chirality):
+
+    grid = np.zeros((n,n),dtype = int)
+    image_status = np.zeros((n,n),dtype = int)
+    counter = 0
+    if grid_type == 'spiral':
+        right = lambda x, y: (x, y+1)
+        left  = lambda x, y: (x, y-1)
+        up    = lambda x, y: (x-1, y)
+        down  = lambda x, y: (x+1, y)
+        direction_mapping = {0:right, 1:up, 2:left, 3:down}
+        # sign_factor = 1-2*invert_chirality
+        counter = counter + 1
+        x = (n)//2
+        y = (n-1+invert_chirality)//2
+        i = 0
+        find_direction = lambda x, invert_chirality:(x+2*invert_chirality)%4 if x%2 == 0 else x%4
+        while (counter < n*n):
+            repeat = (i//2)+1
+            for z in range(repeat):
+                x,y = direction_mapping[find_direction(i, invert_chirality)](x, y)
+                counter = updateGrid(grid, x, y, counter,n)
+            i = i+1
+    elif grid_type == 'normal' or grid_type == 'snake':
+        counter = 0
+        for i in range(n):
+            for j in range(n):
+                grid[i][j] = counter
+                counter = counter + 1
+        if grid_type == 'snake':
+            flip_even_rows = lambda i,x: np.flip(x) if i%2 != 0 else x
+            grid = np.array([flip_even_rows(i,row) for i, row in enumerate(grid)])
+        if invert_chirality:
+            grid = grid.transpose();
+    return grid, image_status
+
+def get_grid(i,j, grid, names):
+    if grid[i][j] < len(names):
+        return grid[i][j]
+    else:
+        return -1
+
+def crop(image, top_left_pixel, bottom_right_pixel):
+    return image[top_left_pixel[0]:bottom_right_pixel[0], top_left_pixel[1]:bottom_right_pixel[1]]
+
+def find_image(i, j, grid, names, image_source, image_status, blank, blank_image, image_dimension, top_left_pixel, bottom_right_pixel, image_input_extension, grayscale_images):
+    path = image_source + get_names(get_grid(i, j, grid, names), names, blank)
+    files = [path + '.' +  extension for extension in image_input_extension]
+    check = [os.path.isfile(file) for file in files]
+    if (sum(check) == 0 or get_grid(i, j, grid, names) == -1):
+        image = blank_image
+    else:
+        file = check.index(True)
+        file = files[file]
+        image = (cv2.imread(file, cv2.IMREAD_GRAYSCALE) if grayscale_images else cv2.imread(file, cv2.IMREAD_COLOR))
+        image_status[i][j] = 1
+    image = cv2.resize(image, image_dimension, interpolation = cv2.INTER_CUBIC)
+    image = crop(image, top_left_pixel, bottom_right_pixel)
+    return image
+
+def concatenate_images(image_grid):
+    return cv2.vconcat([cv2.hconcat(row) for row in image_grid])
+
+def generate_caption(n, names, captions_all, grid, image_status, blank, image_destination):
+    string = "From Left to Right, Top to Bottom\n"
+    sep = ", "
+    for i in range(n):
+        for j in range(n):
+            if image_status[i][j]:
+                string +=  captions_all[get_names(get_grid(i, j, grid, names), names, blank)] + (sep if j < n-1 else "")
+            else:
+                string +=  "_" + (sep if j < n-1 else "")
+        string +=  ("\n" if i < n-1 else "")
+    with open(image_destination + ".txt", 'w') as file:
+        file.write(string)
+
+def generate_image_grid_with_caption(n, grid_type, invert_chirality, captions_all, names_all, blank, blank_image, image_source, image_destination, image_dimension, grid_dimension, top_left_pixel, bottom_right_pixel, image_input_extension, grayscale_images, save_png, save_jpg, force_odd_size):
+    n = 2*n//2 + 1 if force_odd_size else n # To make sure n is odd 
+    names = names_all[:n*n]
+    grid, image_status = generate_grid(n, grid_type, invert_chirality)
+    image_destination +=  str(n) + "by" + str(n)
+    image_grid = [[find_image(i, j, grid, names, image_source, image_status, blank, blank_image, image_dimension, top_left_pixel, bottom_right_pixel, image_input_extension, grayscale_images) for j in range(n)] for i in range(n)]
+    output = concatenate_images(image_grid)
+    image_compressed = cv2.resize(output, grid_dimension, interpolation = cv2.INTER_CUBIC)
+    if save_jpg:
+        cv2.imwrite(image_destination + '.' + 'jpg', output)
+        cv2.imwrite(image_destination + '_compressed.' + 'jpg', image_compressed)
+    if save_png:
+        cv2.imwrite(image_destination + '.' + 'png', output)
+        cv2.imwrite(image_destination + '_compressed.' + 'png', image_compressed)
+    generate_caption(n, names, captions_all, grid, image_status, blank, image_destination)
+
+n, grid_type, invert_chirality, captions_all, names_all, blank, blank_image, image_source, image_destination, image_dimension, grid_dimension, top_left_pixel, bottom_right_pixel, image_input_extension, grayscale_images, save_png, save_jpg, force_odd_size = parse_arguments(arguments)    
+generate_image_grid_with_caption(n, grid_type, invert_chirality, captions_all, names_all, blank, blank_image, image_source, image_destination, image_dimension, grid_dimension, top_left_pixel, bottom_right_pixel, image_input_extension, grayscale_images, save_png, save_jpg, force_odd_size)
